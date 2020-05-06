96dc4430572109cc078ba8b85af7ecc3270c87e1
haiwulei <1021391538@qq.com>
2020-05-06 10:37:44 +0800

    feat: init project

---
+++ README.md
@@ -0,0 +1,5 @@
+## neo dapp
+
+1. Run `yarn` to install dependencies.
+2. Run `yarn start` for local development.
+3. Run `yarn build` to build
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        index: './src/scripts/index.js'
+    },
+    devtool: false,
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader'
+                    },
+                    'sass-loader?sourceMap'
+                ]
+            },
+            {
+                test: /\.(eot|woff2?|svg|ttf)([\?]?.*)$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([{ from: 'src/assets', to: 'assets' }]),
+        new HtmlWebpackPlugin({
+            template: 'src/sites/index.html',
+            inject: 'body',
+            xhtml: true,
+            metadata: {
+                isDevServer: false
+            },
+            minify: {
+                caseSensitive: true,
+                collapseWhitespace: true,
+                keepClosingSlash: true
+            },
+            chunks: 'all',
+            filename: 'index.html'
+        })
+    ],
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].[chunkhash].bundle.js',
+        sourceMapFilename: '[file].map',
+        chunkFilename: '[name].[chunkhash].chunk.js'
+    }
+};
